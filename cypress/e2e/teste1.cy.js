b078822e9e9f7086b72640757e25c54e08c5a8fe
jsvivis <153030744+jsvivis@users.noreply.github.com>
2024-07-22 22:42:53 -0300

    Add #02 - ATIVIDADE CYPRESS - 1

--- cypress/e2e/teste1.cy.js
+++ cypress/e2e/teste1.cy.js
@@ -10,7 +10,7 @@ context('RPVfinance Controls', ()=>{
     //afterEach --> depois de cada teste
 
     beforeEach(()=>{
-         cy.visit('https://dev-finance.netlify.app/');
+         cy.visit('https://dev-finance.netlify.app/'); //endereço da aplicação
     })
 
 
@@ -21,7 +21,7 @@ it('cadastrar entradas', ()=>{
 // adicionar as asserções que vamos precisar
 // cy.get --> mapear um elemento
 
-//endereço da aplicação
+//primeira entrada:
 cy.get('#data-table tbody tr').should('have.length',0)
 cy.get('#transaction .button').click(); //id + class
 //cy.get('#transaction > button')
@@ -32,17 +32,36 @@ cy.get('[type=date]').type('2024-07-17') //atributo
 cy.get('button').contains('Salvar').click(); //tipo
 cy.get('#data-table tbody tr').should('have.length',1)
 
+//segunda entrada:
+cy.get('#transaction .button').click(); //id + class
+cy.get('#description').type('Minhas Compras')
+cy.get('[name=amount]').type('500') //atributo
+cy.get('[type=date]').type('2024-07-22') //atributo
+cy.get('button').contains('Salvar').click(); //tipo
+cy.get('#data-table tbody tr').should('have.length',2)
+
+cy.get('[data-index="0"] > :nth-child(4) > img').click() // removendo lançamentos
 })
 
-it('Cadastrar Saídas', ()=>{
 
-cy.get('#data-table tbody tr').should('have.length',0)
+it.only('Cadastrar Saídas', ()=>{
+
+//primeira saida:
 cy.get('#transaction .button').click(); //id + class
 cy.get('#description').type('Presente Dia dos Pais')
-cy.get('[name=amount]').type('-180') //atributo
+cy.get('[name=amount]').type('-200') //atributo
 cy.get('[type=date]').type('2024-07-18') //atributo
 cy.get('button').contains('Salvar').click(); //tipo
 cy.get('#data-table tbody tr').should('have.length',1)
 
+//segunda saida:
+cy.get('#transaction .button').click(); //id + class
+cy.get('#description').type('Minhas Compras')
+cy.get('[name=amount]').type('-250') //atributo
+cy.get('[type=date]').type('2024-07-18') //atributo
+cy.get('button').contains('Salvar').click(); //tipo
+cy.get('#data-table tbody tr').should('have.length',2)
+
+cy.get('[data-index="1"] > :nth-child(4) > img').click() // removendo lançamentos
 })
 });
